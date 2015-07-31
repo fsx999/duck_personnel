32175342d24b849bbbd5b45b7dabe218df7df942
paul guichon <paul.guichon@iedparis8.net>
2015-07-31 10:56:26 +0200

    mise à jour personnel

--- duck_personnel/migrations/0001_initial.py
+++ duck_personnel/migrations/0001_initial.py
@@ -17,7 +17,8 @@ class Migration(migrations.Migration):
             name='Fonction',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('label', models.CharField(max_length=120, verbose_name='Role')),
+                ('label', models.CharField(max_length=120, verbose_name='Fonction')),
+                ('code', models.CharField(max_length=1, verbose_name='Code Employee', choices=[('R', 'Responsable'), ('E', 'Normale')])),
             ],
         ),
         migrations.CreateModel(
@@ -26,15 +27,15 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('nom', models.CharField(max_length=30, verbose_name='Nom')),
                 ('prenom', models.CharField(max_length=30, verbose_name='Pr\xe9nom')),
-                ('email', models.EmailField(max_length=254, null=True)),
-                ('phone', models.CharField(max_length=10, null=True, verbose_name='T\xe9l\xe9phone')),
+                ('email', models.EmailField(max_length=254, null=True, blank=True)),
+                ('phone', models.CharField(max_length=10, null=True, verbose_name='T\xe9l\xe9phone', blank=True)),
+                ('bureau', models.CharField(max_length=10, null=True, blank=True)),
                 ('lft', models.PositiveIntegerField(editable=False, db_index=True)),
                 ('rght', models.PositiveIntegerField(editable=False, db_index=True)),
                 ('tree_id', models.PositiveIntegerField(editable=False, db_index=True)),
                 ('level', models.PositiveIntegerField(editable=False, db_index=True)),
-                ('fonction', models.ForeignKey(to='duck_personnel.Fonction')),
+                ('fonction', models.ManyToManyField(to='duck_personnel.Fonction', null=True)),
                 ('parent', mptt.fields.TreeForeignKey(related_name='children', blank=True, to='duck_personnel.Personnel', null=True)),
-                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True)),
             ],
             options={
                 'abstract': False,
@@ -44,12 +45,25 @@ class Migration(migrations.Migration):
             name='Service',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('label', models.CharField(max_length=120, verbose_name='Pole')),
+                ('label', models.CharField(max_length=120, verbose_name='Service')),
+                ('lft', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('rght', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('tree_id', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('level', models.PositiveIntegerField(editable=False, db_index=True)),
+                ('parent', mptt.fields.TreeForeignKey(related_name='children', blank=True, to='duck_personnel.Service', null=True)),
             ],
+            options={
+                'abstract': False,
+            },
         ),
         migrations.AddField(
-            model_name='fonction',
+            model_name='personnel',
             name='service',
-            field=models.ForeignKey(to='duck_personnel.Service'),
+            field=models.ForeignKey(to='duck_personnel.Service', null=True),
+        ),
+        migrations.AddField(
+            model_name='personnel',
+            name='user',
+            field=models.ForeignKey(blank=True, to=settings.AUTH_USER_MODEL, null=True),
         ),
     ]
